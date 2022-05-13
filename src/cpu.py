4d5e533713155c9254b718f741f9f1a3186e6b31
Ramon Meza <ramon_meza@live.com>
2022-05-13 11:25:26 -0400

    Settings for CPU

---
+++ src/cpu.py
@@ -0,0 +1,15 @@
+
+class Cpu:
+
+    # variable declarations
+
+
+    # methods
+    def fetch(self) -> None:
+        pass
+
+    def decode(self) -> None:
+        pass
+
+    def execute(self) -> None:
+        pass
